ee3fd7dfbd6466450ef0e6f0f2e3b60f26e85f01
FawziQeshta <45800534+FawziQeshta@users.noreply.github.com>
2025-09-29 15:25:54 +0300

    fixed

--- lib/features/login/ui/widgets/login_bloc_listener.dart
+++ lib/features/login/ui/widgets/login_bloc_listener.dart
@@ -29,7 +29,7 @@ class LoginBlocListener extends StatelessWidget {
           },
           success: (loginResponse) {
             context.pop();
-            context.pushNamed(Routes.homeScreen);
+            context.pushNamedAndRemoveUntil(Routes.homeScreen, predicate: (Route<dynamic> route) => false);
           },
           error: (error) {
             setupErrorState(context, error);
